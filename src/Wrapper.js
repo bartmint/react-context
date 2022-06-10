aec2e757ebec3a8375f31c9e59c65de4f7710b11
bartek <bartek03037@gmail.com>
2022-06-10 23:20:24 +0200

    add extra comment

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -5,14 +5,15 @@ import Kid from "./Kid";
 //context is an alternative to composition
 //it is useful when data is needed at different nesting levels, it allows to reduce unnecessary rendering of parent components by retrieving data by child components directly -> then props are not passed through all components until the target component
 //reduces the serviceability of components
+//<TextContext.Consumer is equivalent to useContext
 
 
 const text = {
   langs: {
     pl:
-      "siema",
+      "pl",
     eng:
-      "hello"
+      "eng"
   }
 }
 
